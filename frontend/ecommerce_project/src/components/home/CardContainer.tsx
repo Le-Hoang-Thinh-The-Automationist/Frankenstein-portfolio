c172b6329623a474865f05e900856e289e6929c7
Mark & Adrien <MarkandAdrien3@gmail.com>
2024-12-09 11:22:07 +0700

    Home page add to the webpage

---
+++ frontend/ecommerce_project/src/components/home/CardContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CardContainer = () => {
+  return (
+    <section className='py-5' id="shop">
+      <h4 style={{textAlign: "center"}}>Our products</h4>
+
+      <div className='container px-4 px-lg-5 mt-5'>
+        <div className='row justify-content-center'>
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CardContainer;
