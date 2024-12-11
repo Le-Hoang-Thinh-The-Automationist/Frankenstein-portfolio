8ef9d4ad06591486d338f15e0e6c831ad4bc3e60
Mark & Adrien <MarkandAdrien3@gmail.com>
2024-12-11 21:39:34 +0700

    fetching API for frontend

--- frontend/ecommerce_project/src/components/home/CardContainer.tsx
+++ frontend/ecommerce_project/src/components/home/CardContainer.tsx
@@ -1,13 +1,17 @@
 import HomeCard from './HomeCard';
 
-const CardContainer = () => {
+type CardContainerProps = {
+  products: string[];
+}
+
+
+const CardContainer : React.FC<CardContainerProps> = ({ products }) => {
   return (
     <section className='py-5' id="shop">
       <h4 style={{textAlign: "center"}}>Our products</h4>
-
       <div className='container px-4 px-lg-5 mt-5'>
         <div className='row justify-content-center'>
-            <HomeCard />
+          {products.map(product => <HomeCard key={product.id} product={product} />)}
         </div>
       </div>
     </section>
--- frontend/ecommerce_project/src/components/home/HomePage.tsx
+++ frontend/ecommerce_project/src/components/home/HomePage.tsx
@@ -1,11 +1,32 @@
 import Header from './Header'
 import CardContainer from './CardContainer'
+import api from '../../api';
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect( () => {
+      
+      // Fetch the data from the http://${api}/products
+      api.get("products")
+      .then(res => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+
+      // If there is an error, catch it
+      .catch(err => {
+        console.log(err.message)
+      }) 
+
+
+    }, [] )
+
   return (
     <>
       <Header />
-      <CardContainer />
+      <CardContainer products={products}/>
     </>
   )
 }
