1552f75bca9b242ed5d3316e705518e57bd3af5e
Mark & Adrien <MarkandAdrien3@gmail.com>
2024-12-15 20:24:04 +0700

    Loading place holder feature

--- frontend/ecommerce_project/src/components/home/HomePage.tsx
+++ frontend/ecommerce_project/src/components/home/HomePage.tsx
@@ -2,22 +2,27 @@ import Header from './Header'
 import CardContainer from './CardContainer'
 import api from '../../api';
 import { useEffect, useState } from 'react';
+import PlaceHolderContainer from '../ui/PlaceHolderContainer';
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect( () => {
       
       // Fetch the data from the http://${api}/products
-      api.get("products")
-      .then(res => {
+      setLoading(true);
+      api.get("products").then(res => {
         console.log(res.data);
         setProducts(res.data);
+        setLoading(false);
       })
 
       // If there is an error, catch it
       .catch(err => {
         console.log(err.message)
+        setLoading(false);
       }) 
 
 
@@ -26,7 +31,8 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      <CardContainer products={products}/>
+      {!loading ? <PlaceHolderContainer /> : <CardContainer products={products}/>}
+      
     </>
   )
 }
